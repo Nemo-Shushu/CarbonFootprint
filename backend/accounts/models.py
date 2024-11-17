6871443a73f4b2d35acb65fc841ebcd9e8ae26c0
atomicsam <61165057+atomicsam@users.noreply.github.com>
2024-11-17 08:45:33 +0000

    Configured display for admin view

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,22 +1,7 @@
 from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    USER_TYPES = (
-      (1, 'Normal'),
-      (2, 'Researcher'),
-      (3, 'Administrator'),
-      )
-
-# # Create your models here.
-# class Users(AbstractUser):
-#     
-
-#     user_role = models.PositiveSmallIntegerField(
-#                   choices=USER_TYPES,
-#                   null=True
-#                   )
-
-#     def __str__(self):
-#         return self.username
+    pass
