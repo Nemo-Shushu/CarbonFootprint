035b5b86d4713980adc19bdcadb8bad24d068bdd
“Xiaoshi <2742059c@student.gla.ac.uk>
2024-11-27 14:14:28 +0000

    Sidebar show in dashboard

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import "./static/Sidebar.css";
+import { useNavigate } from "react-router-dom";
 
 
 const Sidebar = () => {
 
   const [activeItem, setActiveItem] = useState("Dashboard");
 
+  const navigate = useNavigate(); 
+
+    const handleLogout = () => {
+        localStorage.removeItem("userToken"); 
+        navigate("/sign-in");
+    };
+
   return (
     <div className="sidebar">
       <div className="user-info">
       <div className="user-name">
         <span>JOHN</span>
-        <img src="/images/logout.png" alt="Logout Icon" className="icon logout" />
+        <img src="/images/logout.png" alt="Logout Icon" className="icon logout" 
+          onClick={handleLogout}
+          style={{ cursor: "pointer" }}
+        />
       </div>
         <p className="email">2134567@student.gla.ac.uk</p>
         <button className="new-report-btn">+ New Report</button>
