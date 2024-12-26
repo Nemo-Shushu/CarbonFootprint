336a1db1c71e40876960c8285c00a92209ca7ee6
Vladislav Brusokas <brusvlad4002@yandex.ru>
2024-12-27 00:16:05 +0200

    Component names and structure changed, form added to Utilities section

--- frontend/src/calculator.js
+++ frontend/src/calculator.js
@@ -11,13 +11,13 @@ let aggregate = 0;
 //this way, we can avoid multiple calls to the backend, aggregate all information for the report here, without leaving Calculator() component, 
 //and then handleSubmit it all on the last page
 
-function PageZero() {
+function Instructions() {
 
     const navigate = useNavigate();
 
     const handleRoute = () => {
         aggregate += 1
-        navigate("/calculator/page-one")
+        navigate("/calculator/utilities")
     };
 
     return (
@@ -26,72 +26,238 @@ function PageZero() {
             <h5>Use Calculator like that.</h5>
             <h6>{ aggregate }</h6>
             
-            <button className="btn btn-outline-success w-100 py-2 mt-2" type="button" onClick={handleRoute}>
-                Start
-            </button>
+            <div class="d-flex justify-content-end position-fixed bottom-0 end-0 p-3">
+                <button type="button" class="btn btn-success" onClick={handleRoute}>Start</button>
+            </div>
         </main>
     );
 };
 
-function PageOne() {
+function Utilities() {
 
     const navigate = useNavigate();
 
     const handleRoute = () => {
         aggregate += 1
-        navigate("/calculator/page-two")
+        navigate("/calculator/travel")
     };
 
     return (
         <main class="ms-sm-auto px-md-4">
-            <h2>Page One</h2>
-            <h6>{ aggregate }</h6>
+            <h2>Utilities</h2>
 
-            <button className="btn btn-outline-success w-100 py-2 mt-2" type="button" onClick={handleRoute}>
-                Next Page
-            </button>
+            <form className="needs-validation" noValidate>
+                <div className="row g-2">
+                    <div className="mt-5">
+                        <strong>
+                            Personnel:
+                        </strong>
+                    </div>
+
+                    <hr/>
+
+                    <div className="col-sm-4">
+                        <label htmlFor="firstName" className="form-label"><strong>Number of FTE staff working on project</strong></label>
+                        <input type="number" className="form-control" id="FTE-staff" placeholder="Enter number of people" required />
+                        <div className="invalid-feedback">
+                        Valid number is required.
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4">
+                        <label htmlFor="firstName" className="form-label"><strong>Total number of FTE research group members</strong></label>
+                        <input type="number" className="form-control" id="FTE-members" placeholder="Enter number of people" required />
+                        <div className="invalid-feedback">
+                        Valid number is required.
+                        </div>
+                    </div>
+                    
+                    <div className="mt-5">
+                        <strong>
+                            Type of space (for calculation of electricity and gas consumption):
+                        </strong>
+                    </div>
+
+                    <hr/>
+
+                    <div className="col-sm-4">
+                        <label htmlFor="firstName" className="form-label"><strong>Academic laboratory</strong></label>
+                        <input type="number" className="form-control" id="academic-laboratory-area" placeholder="Enter area in sq. ft." required />
+                        <div className="invalid-feedback">
+                        Valid number is required.
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4">
+                        <label htmlFor="firstName" className="form-label"><strong>Admin office</strong></label>
+                        <input type="number" className="form-control" id="admin-office-area" placeholder="Enter area in sq. ft." required />
+                        <div className="invalid-feedback">
+                        Valid number is required.
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4">
+                        <label htmlFor="firstName" className="form-label"><strong>Academic office</strong></label>
+                        <input type="number" className="form-control" id="academic-office-area" placeholder="Enter area in sq. ft." required />
+                        <div className="invalid-feedback">
+                        Valid number is required.
+                        </div>
+                    </div>
+
+                    <div className="mt-5">
+                        <strong>
+                            Type of space (for calculation of water consumption):
+                        </strong>
+                    </div>
+
+                    <hr/>
+
+                    <div className="col-sm-4">
+                        <label htmlFor="firstName" className="form-label"><strong>Physical sciences laboratory</strong></label>
+                        <input type="number" className="form-control" id="physical-laboratory-area" placeholder="Enter area in sq. ft." required />
+                        <div className="invalid-feedback">
+                        Valid number is required.
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4">
+                        <label htmlFor="firstName" className="form-label"><strong>Engineering laboratory</strong></label>
+                        <input type="number" className="form-control" id="engineering-laboratory-area" placeholder="Enter area in sq. ft." required />
+                        <div className="invalid-feedback">
+                        Valid number is required.
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4">
+                        <label htmlFor="firstName" className="form-label"><strong>Medical/Life sciences laboratory</strong></label>
+                        <input type="number" className="form-control" id="medical-laboratory-area" placeholder="Enter area in sq. ft." required />
+                        <div className="invalid-feedback">
+                        Valid number is required.
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4">
+                        <label htmlFor="firstName" className="form-label"><strong>Office/Admin space</strong></label>
+                        <input type="number" className="form-control" id="admin-space-area" placeholder="Enter area in sq. ft." required />
+                        <div className="invalid-feedback">
+                        Valid number is required.
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+
+            <div class="d-flex justify-content-end position-fixed bottom-0 end-0 p-3">
+                <button type="button" class="btn btn-success" onClick={handleRoute}>Next</button>
+            </div>
         </main>
     );
 };
 
-function PageTwo() {
+function Travel() {
 
     const navigate = useNavigate();
 
+    const handleBack = () => {
+        aggregate += 1
+        navigate("/calculator/utilities")
+    };
+
     const handleRoute = () => {
         aggregate += 1
-        navigate("/calculator/page-three")
+        navigate("/calculator/waste")
     };
 
     return (
         <main class="ms-sm-auto px-md-4">
-            <h2>Page Two</h2>
+            <h2>Travel</h2>
             <h6>{ aggregate }</h6>
             
-            <button className="btn btn-outline-success w-100 py-2 mt-2" type="button" onClick={handleRoute}>
-                Next Page
-            </button>
+            <div class="d-flex justify-content-end position-fixed bottom-0 end-0 p-3">
+                <button type="button" class="btn btn-outline-secondary me-2" onClick={handleBack}>Back</button>
+                <button type="button" class="btn btn-success" onClick={handleRoute}>Next</button>
+            </div>
         </main>
     );
 };
 
-function PageThree() {
+function Waste() {
 
     const navigate = useNavigate();
 
+    const handleBack = () => {
+        aggregate += 1
+        navigate("/calculator/travel")
+    };
+
     const handleRoute = () => {
+        aggregate += 1
+        navigate("/calculator/procurement")
+    };
+
+    return (
+        <main class="ms-sm-auto px-md-4">
+            <h2>Waste</h2>
+            <h6>{ aggregate }</h6>
+            
+            <div class="d-flex justify-content-end position-fixed bottom-0 end-0 p-3">
+                <button type="button" class="btn btn-outline-secondary me-2" onClick={handleBack}>Back</button>
+                <button type="button" class="btn btn-success" onClick={handleRoute}>Next</button>
+            </div>
+        </main>
+    );
+};
+
+function Procurement() {
+
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        aggregate += 1
+        navigate("/calculator/waste")
+    };
+
+    const handleRoute = () => {
+        aggregate += 1
+        navigate("/calculator/results")
+    };
+
+    return (
+        <main class="ms-sm-auto px-md-4">
+            <h2>Procurement</h2>
+            <h6>{ aggregate }</h6>
+            
+            <div class="d-flex justify-content-end position-fixed bottom-0 end-0 p-3">
+                <button type="button" class="btn btn-outline-secondary me-2" onClick={handleBack}>Back</button>
+                <button type="button" class="btn btn-success" onClick={handleRoute}>Next</button>
+            </div>
+        </main>
+    );
+};
+
+function Results() {
+
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        aggregate += 1
+        navigate("/calculator/procurement")
+    };
+
+    const handleSubmit = () => {
         aggregate = 0
         navigate("/dashboard")
     };
 
     return (
         <main class="ms-sm-auto px-md-4">
-            <h2>Page Three</h2>
+            <h2>Results</h2>
             <h6>{ aggregate }</h6>
             
-            <button className="btn btn-outline-success w-100 py-2 mt-2" type="button" onClick={handleRoute}>
-                Submit
-            </button>
+            <div class="d-flex justify-content-end position-fixed bottom-0 end-0 p-3">
+                <button type="button" class="btn btn-outline-secondary me-2" onClick={handleBack}>Back</button>
+                <button type="button" class="btn btn-success" onClick={handleSubmit}>Submit</button>
+            </div>
         </main>
     );
 };
@@ -105,10 +271,12 @@ function Calculator() {
                     <h1 className="h2">Calculator</h1>
                 </div>
                 <Routes>
-                    <Route path="/" element={<PageZero />} />
-                    <Route path="page-one" element={<PageOne />} />
-                    <Route path="page-two" element={<PageTwo />} />
-                    <Route path="page-three" element={<PageThree />} />
+                    <Route path="/" element={<Instructions />} />
+                    <Route path="utilities" element={<Utilities />} />
+                    <Route path="travel" element={<Travel />} />
+                    <Route path="waste" element={<Waste />} />
+                    <Route path="procurement" element={<Procurement />} />
+                    <Route path="results" element={<Results />} />
                 </Routes>
             </main>
         </div>
