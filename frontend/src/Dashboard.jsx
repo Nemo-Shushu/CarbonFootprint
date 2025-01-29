3924177e55b771fe160065fba7397670828cd792
Vladislav Brusokas <brusvlad4002@yandex.ru>
2025-01-29 12:17:53 +0000

    Reverted change to Dashboard - useAuth conditional returned - handleProtect returned - now, if user navigates to Dashboard without logging in, they are routed back to sign-in

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -79,7 +79,7 @@ function Dashboard() {
         setShowProfile(queryParams.get("showProfile") === "true");
       }, [location.search]);
 
-    return (
+      return useAuth() ? (
         <div style={{ display: "flex", height: "100vh" }}>
             <Sidebar style={{ flex: "0 0 17%",}} onNameClick={toggleProfile}/>
             <main style={{ flex: "1", padding: "1rem", overflowY: "auto",}} onClick={() => setShowProfile(false)}>
@@ -92,7 +92,9 @@ function Dashboard() {
                 <TableComponent />
             </main>
         </div>
-    )
+    ) : (
+        handleProtect()
+    );
 }
 
 export { Dashboard, TableComponent };
