f6efaf18224694a1c8f675cecdcc0e2c5cb413d1
Taewan Kim <2798185k@stduent.gla.ac.uk>
2025-01-29 17:24:46 +0000

    fixed pk issue caused by models - made name field a pk

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -7,14 +7,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class University(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, primary_key=True)
 
     def __str__(self):
         return self.name
 
 
 class ResearchField(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True, primary_key=True)
 
     def __str__(self):
         return self.name
