aed4b00f9464c2c8ac18a422dec92984d9b2bae3
Taewan Kim <2798185k@stduent.gla.ac.uk>
2025-03-12 06:40:11 +0000

    Ammed RegisterSerializer and add CreateUserSerializer

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -103,6 +103,104 @@ class RegisterSerializer(serializers.ModelSerializer):
             )
         return value
 
+class ConversionFactorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConversionFactor
+        fields = "__all__"
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        error_messages={"blank": "Email is empty. Please fill in the field."},
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="A user with this email already exists.",
+            )
+        ],
+    )
+
+    username = serializers.CharField(
+        required=True,
+        error_messages={"blank": "Username is empty. Please fill in the field."},
+    )
+
+    first_name = serializers.CharField(
+        required=True,
+        error_messages={"blank": "first name is empty. Please fill in the field."},
+    )
+
+    last_name = serializers.CharField(
+        required=True,
+        error_messages={"blank": "last name is empty. Please fill in the field."},
+    )
+
+    institute = serializers.SlugRelatedField(
+        slug_field="name",
+        queryset=University.objects.all(),
+        required=True,
+        error_messages={"null": "Instutution is empty. Please fill in the field."},
+    )
+
+    research_field = serializers.SlugRelatedField(
+        slug_field="name",
+        queryset=ResearchField.objects.all(),
+        required=True,
+        error_messages={"null": "Research Field is empty. Please fill in the field."},
+    )
+
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={"input_type": "password"},
+        error_messages={"blank": "Password is empty. Please fill in the field."},
+    )
+
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={"input_type": "password"},
+        error_messages={
+            "blank": "Password confirmation is empty. Please fill in the field."
+        },
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "password",
+            "password2",
+            "email",
+            "institute",
+            "research_field",
+            "first_name",
+            "last_name",
+        )
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+
+        return attrs
+
+    def validate_email(self, value):
+        if not value.lower().endswith(".ac.uk"):
+            raise serializers.ValidationError(
+                "Email must belong to an educational institution (.ac.uk)."
+            )
+        return value
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                "A user with this username already exists."
+            )
+        return value
+
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data["username"],
@@ -115,9 +213,3 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data["password"])
         user.save()
         return user
-
-
-class ConversionFactorsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ConversionFactor
-        fields = "__all__"
